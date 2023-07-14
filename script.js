d2c9693b063a91f9fade3516e55b778ae8dd3768
Sasuke <118628015+Sasukee11@users.noreply.github.com>
2023-07-14 23:12:09 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,19 @@
 //your JS code here. If required.
+function getWindowSize() {
+  const width = window.innerWidth || document.documentElement.clientWidth || document.body.clientWidth;
+  const height = window.innerHeight || document.documentElement.clientHeight || document.body.clientHeight;
+
+  const sizeInfoDiv = document.getElementById("sizeInfo");
+  if (sizeInfoDiv) {
+    const sizeInfoH1 = sizeInfoDiv.querySelector("h1");
+    if (sizeInfoH1) {
+      sizeInfoH1.textContent = `Width: ${width} and Height: ${height}`;
+    }
+  }
+}
+
+// Get initial window size
+getWindowSize();
+
+// Add event listener for window resize
+window.addEventListener('resize', getWindowSize);
